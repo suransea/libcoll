4351c69f64575264f7014f19f34013506d8c9522
sea <simpleslight@icloud.com>
2019-12-03 18:54:53 +0800

    add interface

--- src/deque.h
+++ src/deque.h
@@ -5,4 +5,33 @@
 #ifndef LIBCOLL_DEQUE_H
 #define LIBCOLL_DEQUE_H
 
+#include <stddef.h>
+#include <stdbool.h>
+
+typedef struct _deque Deque;
+
+Deque *deque_new();
+
+void deque_append(Deque *deque, void *data);
+
+void deque_prepend(Deque *deque, void *data);
+
+size_t deque_len(Deque *deque);
+
+void *deque_at(Deque *deque, int index);
+
+void *deque_assign(Deque *deque, int index, void *data);
+
+void *deque_remove_front(Deque *deque);
+
+void *deque_remove_back(Deque *deque);
+
+void deque_foreach(Deque *deque, void(*visit)(void *));
+
+bool deque_empty(Deque *deque);
+
+void deque_clear(Deque *deque);
+
+void deque_free(Deque *deque);
+
 #endif //LIBCOLL_DEQUE_H
--- src/map.h
+++ src/map.h
@@ -5,4 +5,38 @@
 #ifndef LIBCOLL_MAP_H
 #define LIBCOLL_MAP_H
 
+#include <stdbool.h>
+#include <stddef.h>
+#include "seq.h"
+
+typedef struct _map Map;
+
+Map *map_new();
+
+Map *map_new_custom(int (*cmp)(void *, void *));
+
+void *map_insert(Map *map, void *key, void *value);
+
+void *map_value_of(Map *map, void *key);
+
+Seq *map_keys(Map *map);
+
+Seq *map_values(Map *map);
+
+size_t map_size(Map *map);
+
+bool map_empty(Map *map);
+
+bool map_contains_key(Map *map, void *key);
+
+bool map_contains_value(Map *map, void *value);
+
+void *map_remove(Map *map, void *key);
+
+void map_clear(Map *map);
+
+void map_foreach(Map *map, void(*visit)(void *key, void *value));
+
+void map_free(Map *map);
+
 #endif //LIBCOLL_MAP_H
